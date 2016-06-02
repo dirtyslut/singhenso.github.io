3009940b9316e488e45b8bd76583a564c836656b
Gagandeep Singh <singh.enso@gmail.com>
2016-06-01 18:25:06 -0700

    added CNAME

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+ });
+
+  $('#name').hover(function() {
+    $main_text=$(this).text();
+    $(this).text("Welcome to my Website").fadeIn();
+  }, function() {
+    $(this).text($main_text);
+
+  });
+
+  $('nav a').click(function () {
+   var $href = $(this).attr('href');
+   $('body').stop().animate({
+     scrollTop: $($href).offset().top
+   }, 1000);
+   return false;
+  });
+
+
+
+$('.carousel').carousel()
